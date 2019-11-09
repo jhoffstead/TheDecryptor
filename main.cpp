85a435651b5b695e1954ff5f510b4290a8a5d531
Greg <77gregory07@gmail.com>
2019-11-08 22:30:15 -0600

    Added switch statement. Need to work on toupper.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,38 @@
  */
 
  #include <iostream>
+
+#include "cctype"//toupper
 using namespace std;
- void menu(int n);
+ void menu();
 
 int main(){
-    menu(1);
+    string pswd;
+    char choice;
+
+    menu();
+    cin >> choice;
+    toupper(choice);//Need to fix this
+    do {
+        switch (choice) {
+            case 'A'://call encrypt
+                break;
+            case 'B'://call decrypt
+                break;
+            case 'Q':
+                break;
+            default:
+                cout << "Invalid Choice. Try Again.\n";
+        }
+        menu();
+        cin >> choice;
+        toupper(choice);
+    }while(choice != 'Q' || choice != 'q');
     return 0;
 }
 void menu(){
-    cout << "Welcome to The Decrypter 1.0";
+    cout << "Welcome to The Decrypter 1.0\n";
+    cout << "A) Encrypt a password\n";
+    cout << "B) Decrypt a password\n";
+    cout << "Q) Quit\n";
 }
